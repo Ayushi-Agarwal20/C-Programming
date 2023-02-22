bd4271d830c1ce47dc747d066430b510aea30e07
Ayushi-Agarwal20 <115617319+Ayushi-Agarwal20@users.noreply.github.com>
2023-02-22 21:17:47 +0530

    Update leapyear.c

--- Conditional Operators/leapyear.c
+++ Conditional Operators/leapyear.c
@@ -4,17 +4,6 @@ int main()
 {
     int year;
     scanf("%d", &year);
-    if(year % 400 == 0 || year % 4 == 0)
-    {
-        printf("It is a leap year");
-    }
-    else if(year % 100 == 0)
-    {
-        printf("It is not a leap year");
-    }
-    else
-    {
-        printf("It is not a leap year");
-    }
+    year % 400 == 0 || year % 4 == 0 ? printf("It is a leap year") : printf("It is not a leap year");
     return 0;
 }
